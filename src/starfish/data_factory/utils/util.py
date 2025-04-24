72f5cf6be2c52da99041cfd7750cabcc707242e9
johnwayne.jiang <johnwayne.jiang2024@gmail.com>
2025-04-23 18:39:04 -0700

    update telemetry run_time_platform

--- src/starfish/data_factory/utils/util.py
+++ src/starfish/data_factory/utils/util.py
@@ -1,3 +1,6 @@
+import os
+
+
 def get_platform_name() -> str:
     """Check if the code is running in a Jupyter notebook."""
     try:
@@ -5,6 +8,9 @@ def get_platform_name() -> str:
         from IPython import get_ipython
 
         shell = get_ipython().__class__.__name__
+        # Check for VS Code specific environment variables
+        if "VSCODE_PID" in os.environ or "VSCODE_CWD" in os.environ:
+            shell = "VSCodeShell"
         return shell
 
     except:
